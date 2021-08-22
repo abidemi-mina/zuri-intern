a0a42168193f708560968df1032ecfc67cb50fd2
abidemi-mina <aminatabidemi212@gmail.com>
2021-08-21 20:25:53 -0700

    'aminat'

--- ZuriIntern/settings.py
+++ ZuriIntern/settings.py
@@ -16,8 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS =[STATIC_DIR,] 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -124,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
